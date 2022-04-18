3e520abd7d953571c3584a7652de0edc778a16fe
kikiherliawan <kikiherliawann@gmail.com>
2022-04-18 11:56:44 +0700

    pertemuan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <img src="https://cdn.kibrispdr.org/data/gambar-animasi-wajah-6.jpg" alt="flowers" class="avatar">
+
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Kiki</li>
+ <li>Nama Belakang : Herliawan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> The Castilla</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Lampung</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Ngoding</li>
+ <li>Trading</li>
+ </ul>
+</section>
